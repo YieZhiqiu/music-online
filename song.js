031a55bcc58449fefb70ef51de0eb8541d254e1e
YieZhiqiu <30974111+YieZhiqiu@users.noreply.github.com>
2019-02-02 20:32:37 +0800

    Update song.js

--- song.js
+++ song.js
@@ -108,7 +108,7 @@ $(function(){
 		$('.page').attr('style','background-image: url("' + backgroundUrl + '")')
 		
 		
-        $('.disc > .disc-picture').attr('src', img)
+        $('.disc > 。disc-container > .disc-picture').attr('src', img)
 
 		$('.song-description > h2').text(name) 
 		parseLyric(lyric)
